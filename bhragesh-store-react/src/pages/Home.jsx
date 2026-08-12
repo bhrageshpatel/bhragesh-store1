415adca54fdeeeff55563457c3fe6f3abd27f8cd
bhrageshpatel <bhrageshpatel@gmail.com>
2026-08-12 22:25:17 +0530

    Remove unused duplicate categories component

--- bhragesh-store-react/src/pages/Home.jsx
+++ bhragesh-store-react/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 
 import Hero from "../components/hero/Hero";
 import Products from "../components/products/Products";
-import Categories from "../components/categories/categories";
 // import Footer from "../components/layout/Footer";
 
 export default function Home() {
